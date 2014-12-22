2b4489a159c0b1f561eb2f56d57cc85382264809
naeemattari7@gmail.com <Muhammad Naeem>
2014-12-23 01:38:15 +0500

    delete all

--- protected/yiic.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// change the following paths if necessary
-$yiic='E:\softwares\yii stuff\yii_framework\framework\yiic.php';
-$config=dirname(__FILE__).'/config/console.php';
-
-require_once($yiic);
